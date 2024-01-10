862c89dd69b870096be61392288c5fd5ebe8a39f
Swetha Gunnam <swethagunnam@Swethas-MacBook-Pro.local>
2024-01-10 14:56:47 -0500

    third commit for basic.js

---
+++ basic.js
@@ -0,0 +1,12 @@
+const person = {
+  name: "swetha",
+  age: 30,
+  showName() {
+    console.log(this);
+    console.log(this.name);
+    console.log("hai");
+    console.log("hai1");
+  },
+};
+
+person.showName();
